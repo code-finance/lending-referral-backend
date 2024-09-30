4d5d9a079e3e71d8b253fce8f094cd29a29e98ab
Peter H. Nahm <hwnahm@gmail.com>
2024-09-30 14:23:11 +0900

    fix: change API name

--- src/lib/swaggerDoc.js
+++ src/lib/swaggerDoc.js
@@ -3,9 +3,9 @@ const swaggerDoc = require('swagger-jsdoc');
 const swaggerDefinition = {
   openapi: '3.0.1',
   info: {
-    title: 'Kommune Referral Program API',
+    title: 'Lending Referral Program API',
     version: '1.0.0',
-    description: 'Kommune Referral Program API Docs',
+    description: 'Lending Referral Program API Docs',
   },
   host: `localhost:${process.env.PORT}`,
   basePath: '/',
